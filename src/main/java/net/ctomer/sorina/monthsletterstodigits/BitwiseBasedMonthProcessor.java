145e4b817a31c6be34375fd111d2fbd8fdcef0cb
Sorina Grave <sorinagrave@gmail.com>
2015-11-29 10:35:13 +0000

    Reduced more variables

--- src/main/java/net/ctomer/sorina/monthsletterstodigits/BitwiseBasedMonthProcessor.java
+++ src/main/java/net/ctomer/sorina/monthsletterstodigits/BitwiseBasedMonthProcessor.java
@@ -7,22 +7,20 @@ public class BitwiseBasedMonthProcessor implements MonthProcessor {
     private static final int SMALLEST_THREE_DIGIT_31_MULTIPLE = 124;
     private static final int BIGGEST_THREE_DIGIT_31_MULTIPLE = 992;
 
-    private int aLetterBitMask;
-    private int aDigit;
+    private char aDigitChar;
     private int janNumber;
 
     public void findFebruary(){
         for(janNumber = SMALLEST_THREE_DIGIT_31_MULTIPLE; janNumber <= BIGGEST_THREE_DIGIT_31_MULTIPLE; janNumber += 31) {
             // this is JAN
             char[] janDigits = String.valueOf(janNumber).toCharArray();
-            aDigit = Character.getNumericValue(janDigits[1]);
-            aLetterBitMask = 0x1 << aDigit;
-            // we know R = 0, add it to the cumulative map to begin each combination
+            aDigitChar = janDigits[1];
+            // we know R = 0, add it to the cumulative map in position 0 to begin each combination
             int currentMaskBitMap = 0x1;
             for (char c : janDigits) {
                 int cMask = (0x1 << Character.getNumericValue(c));
                 if((cMask & currentMaskBitMap) > 0){
-                    // digit repeating - exclude this combo and reset mask
+                    // digit repeating - exclude this combination and reset the mask
                     currentMaskBitMap = 0x1;
                     break;
                 }
@@ -32,7 +30,10 @@ public class BitwiseBasedMonthProcessor implements MonthProcessor {
             if(currentMaskBitMap != 0x1){
                 // currentMaskBitMap hasn't been reset, therefore it is valid
                 int febNumber = processMayMask(currentMaskBitMap);
-                if(febNumber > 0) return;
+                if(febNumber > 0){
+                    System.out.println("FEB = " + febNumber);
+                    return;
+                }
             }
         }
     }
@@ -44,13 +45,13 @@ public class BitwiseBasedMonthProcessor implements MonthProcessor {
             }
             char[] mayDigits = String.valueOf(mayNumber).toCharArray();
             // second letter must be A
-            if((aLetterBitMask & (0x1 << Character.getNumericValue(mayDigits[1]))) != aLetterBitMask){
+            if(mayDigits[1] != aDigitChar){
                 continue;
             }
             int currentMaskBitMap = currentJanMaskBitMap;
             for (int k = 0; k < mayDigits.length; k++) {
                 if(k == 1){
-                    // second letter is A, this is added already to the mask, skip it
+                    // second letter should be A, this is added already to the mask, skip it
                     continue;
                 }
                 int cMask = (0x1 << Character.getNumericValue(mayDigits[k]));
@@ -71,19 +72,15 @@ public class BitwiseBasedMonthProcessor implements MonthProcessor {
         return -1;
     }
     private int processAprMask(int currentJanMayMaskBitMap){
-        //Find the last 4 unused digits to identify P in APR - XOR on map
-        int unusedDigits = ~currentJanMayMaskBitMap;
-
         for(int k = 1; k < 10; k++){
-            // take the remaining digits one by one except for zero where we know it will not be valid
+            // take the unused mask digits one by one except for zero where we know it will not be valid
             int pLetterBitMask = (0x1 << k);
-            if((pLetterBitMask & unusedDigits) > 0){
+            if((pLetterBitMask & currentJanMayMaskBitMap) == 0){
                 // if current digit added to "A" digit is divisible by 3, that is'P'
-                if((k + aDigit)%3 == 0){
+                if((k + Character.getNumericValue(aDigitChar))%3 == 0){
                     currentJanMayMaskBitMap |= pLetterBitMask;
                     int nFeb = processFebMask(currentJanMayMaskBitMap);
                     if(nFeb > 0){
-                        System.out.println("FEB = " + nFeb);
                         return nFeb;
                     }
                 }
@@ -92,55 +89,47 @@ public class BitwiseBasedMonthProcessor implements MonthProcessor {
         return -1;
     }
     private int processFebMask(int currentJanMayAprMaskBitMap){
-        //Find the last 4 unused digits to identify P in APR - XOR on map
-        int unusedDigits = ~currentJanMayAprMaskBitMap;
         int nCurrent = 0;
         int[] febDigits = new int[3];
         for(int k = 1; k < 10; k++){
             // take the remaining digits one by one by applying 1-9 mask
-            if(((0x1 << k) & unusedDigits) > 0){
+            if(((0x1 << k) & currentJanMayAprMaskBitMap) == 0){
                 febDigits[nCurrent] = k;
                 nCurrent++;
             }
         }
-        return getValid28Combination(febDigits);
+        int febNum = processFebCombination(febDigits, 1, 0, 2);
+        if (febNum > 0) {
+            return febNum;
+        }
+        febNum = processFebCombination(febDigits, 1, 2, 0);
+        if (febNum > 0) {
+            return febNum;
+        }
+        febNum = processFebCombination(febDigits, 0, 1, 2);
+        if (febNum > 0) {
+            return febNum;
+        }
+        febNum = processFebCombination(febDigits, 0, 2, 1);
+        if (febNum > 0) {
+            return febNum;
+        }
+        febNum = processFebCombination(febDigits, 2, 1, 0);
+        if (febNum > 0) {
+            return febNum;
+        }
+        febNum = processFebCombination(febDigits, 2, 0, 1);
+        if (febNum > 0) {
+            return febNum;
+        }
+        return -1;
     }
-    // ideally generate permutations programmatically by recursion
-    private int getValid28Combination(int[] febDigits){
-        int febNumber;
-        if((febDigits[2] & 0x11) == 0 || ((febDigits[2] & 0x11) == 0x10 && (febDigits[0] & 0x1) == 0x1)){
-            febNumber = febDigits[1]*100 + febDigits[0]*10 + febDigits[2];
-            if(febNumber % 7 == 0){
-                return febNumber;
-            }
-        }
-        if((febDigits[2] & 0x11) == 0 || ((febDigits[2] & 0x11) == 0x10 && (febDigits[1] & 0x1) == 0x1)){
-            febNumber = febDigits[0]*100 + febDigits[1]*10 + febDigits[2];
-            if(febNumber % 7 == 0){
-                return febNumber;
-            }
-        }
-        if((febDigits[0] & 0x11) == 0 || ((febDigits[0] & 0x11) == 0x10 && (febDigits[1] & 0x1) == 0x1)){
-            febNumber = febDigits[2]*100 + febDigits[1]*10 + febDigits[0];
-            if(febNumber % 7 == 0){
-                return febNumber;
-            }
-        }
-        if((febDigits[0] & 0x11) == 0 || ((febDigits[0] & 0x11) == 0x10 && (febDigits[2] & 0x1) == 0x1)){
-            febNumber = febDigits[1]*100 + febDigits[2]*10 + febDigits[0];
-            if(febNumber % 7 == 0){
-                return febNumber;
-            }
-        }
-        if((febDigits[1] & 0x11) == 0 || ((febDigits[1] & 0x11) == 0x10 && (febDigits[2] & 0x1) == 0x1)){
-            febNumber = febDigits[0]*100 + febDigits[2]*10 + febDigits[1];
-            if(febNumber % 7 == 0){
-                return febNumber;
-            }
-        }
-        if((febDigits[1] & 0x11) == 0 || ((febDigits[1] & 0x11) == 0x10 && (febDigits[0] & 0x1) == 0x1)){
-            febNumber = febDigits[2]*100 + febDigits[0]*10 + febDigits[1];
-            if(febNumber % 7 == 0){
+    private int processFebCombination (int[] febDigits, int i, int j, int k){
+        // divisibility by 4 checked from the last 2 digits
+        if(((febDigits[k] & 0x11) == 0 && (febDigits[j] & 0x1) == 0) ||
+                ((febDigits[k] & 0x11) == 0x10 && (febDigits[j] & 0x1) == 0x1)) {
+            int febNumber = febDigits[i] * 100 + febDigits[j] * 10 + febDigits[k];
+            if (febNumber % 7 == 0) {
                 return febNumber;
             }
         }
